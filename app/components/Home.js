a6bbd776ccece172b1146431b64bf5314d449f30
Henrik Raitasola <heka.fof@gmail.com>
2016-01-09 14:26:46 +0200

    Changed Home component to Stateless Function Component. Use this syntax if components has no state and it has only render function.

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,50 +1,50 @@
 import React, { Component } from 'react';
 import { Button, Glyphicon } from 'react-bootstrap';
 
-export default class Home extends Component {
-	render() {
-		return (
-			<div>
-				<header className="home-header">
-					<div className="home-header--container container">
-						<div className="row">
-							<div className="col-sm-5 col-sm-push-7 col-xs-10 col-xs-push-1">
-								<div className="home-header--welcome-container">
-									<h1 className="home-header--heading">Gym Diary</h1>
-									<p className="home-header--description">Write your gym results here with mobile phone. Notebooks are in the past!</p>
-								</div>
+const Home = function() {
+	return (
+		<div>
+			<header className="home-header">
+				<div className="home-header--container container">
+					<div className="row">
+						<div className="col-sm-5 col-sm-push-7 col-xs-10 col-xs-push-1">
+							<div className="home-header--welcome-container">
+								<h1 className="home-header--heading">Gym Diary</h1>
+								<p className="home-header--description">Write your gym results here with mobile phone. Notebooks are in the past!</p>
 							</div>
 						</div>
 					</div>
-				</header>
-				<div className="container">
-					<div className="main-content">
-						<div className="row">
-							<div className="col-sm-4">
-								<div className="feature">
-									<Glyphicon className="feature--icon" glyph="phone" />
-									<h3 className="feature--heading">Mobile</h3>
-									<p className="feature--description">Laboris ex. Est facilisis molestie. Inceptos eros cillum. Commodo iaculis. Veniam viverra. Malesuada penatibus aptent.</p>
-								</div>
+				</div>
+			</header>
+			<div className="container">
+				<div className="main-content">
+					<div className="row">
+						<div className="col-sm-4">
+							<div className="feature">
+								<Glyphicon className="feature--icon" glyph="phone" />
+								<h3 className="feature--heading">Mobile</h3>
+								<p className="feature--description">Laboris ex. Est facilisis molestie. Inceptos eros cillum. Commodo iaculis. Veniam viverra. Malesuada penatibus aptent.</p>
 							</div>
-							<div className="col-sm-4">
-								<div className="feature">
-									<Glyphicon className="feature--icon" glyph="ok" />
-									<h3 className="feature--heading">Consistent</h3>
-									<p className="feature--description">Laboris ex. Est facilisis molestie. Inceptos eros cillum. Commodo iaculis. Veniam viverra. Malesuada penatibus aptent.</p>
-								</div>
+						</div>
+						<div className="col-sm-4">
+							<div className="feature">
+								<Glyphicon className="feature--icon" glyph="ok" />
+								<h3 className="feature--heading">Consistent</h3>
+								<p className="feature--description">Laboris ex. Est facilisis molestie. Inceptos eros cillum. Commodo iaculis. Veniam viverra. Malesuada penatibus aptent.</p>
 							</div>
-							<div className="col-sm-4">
-								<div className="feature">
-									<Glyphicon className="feature--icon" glyph="stats" />
-									<h3 className="feature--heading">Insightful</h3>
-									<p className="feature--description">Laboris ex. Est facilisis molestie. Inceptos eros cillum. Commodo iaculis. Veniam viverra. Malesuada penatibus aptent.</p>
-								</div>
+						</div>
+						<div className="col-sm-4">
+							<div className="feature">
+								<Glyphicon className="feature--icon" glyph="stats" />
+								<h3 className="feature--heading">Insightful</h3>
+								<p className="feature--description">Laboris ex. Est facilisis molestie. Inceptos eros cillum. Commodo iaculis. Veniam viverra. Malesuada penatibus aptent.</p>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
 }
+
+export default Home;
