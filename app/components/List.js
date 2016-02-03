09978c701269a5d88b5de5e0ed464688aedcdede
Henrik Raitasola <heka.fof@gmail.com>
2016-02-03 02:13:54 +0200

    Removed class because no state is needed here.

--- app/components/List.js
+++ app/components/List.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import { Link } from 'react-router';
 
-export default class List extends Component {
-	render() {
-		let items = this.props.data.map(item => {
-			return <ListGroupItem key={item._id}><Link to={`/${this.props.linkTo}/${item._id}`} query={{name: item.name}}>{item.name}</Link></ListGroupItem>;
-		});
+const List = ({ data, linkTo }) => {
+	const items = data.map(item => {
 		return (
-			<ListGroup>
-        {items}
-      </ListGroup>
+			<ListGroupItem key={item._id}>
+				<Link to={`/${linkTo}/${item._id}`} query={{name: item.name}}>{item.name}</Link>
+			</ListGroupItem>
 		);
-	}
+	});
+	return (
+		<ListGroup>
+      {items}
+    </ListGroup>
+	);
 }
+
+export default List;
