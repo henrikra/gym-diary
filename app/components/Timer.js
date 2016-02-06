dcb6490f6c8cd134f752b2af2ff94823a0f3897f
Henrik Raitasola <heka.fof@gmail.com>
2016-02-06 23:09:24 +0200

    Cleaned up timer.

--- app/components/Timer.js
+++ app/components/Timer.js
@@ -8,54 +8,45 @@ export default class Timer extends Component {
     timerRunning: false
   }
   setInitialTime = (event) => {
-    if (!this.state.timerRunning) {
-      this.setState({
-      	selectedSeconds: event.target.options[event.target.selectedIndex].value,
-      	secondsRemaining: event.target.options[event.target.selectedIndex].value
+    this.setState({
+    	selectedSeconds: event.target.options[event.target.selectedIndex].value,
+    	secondsRemaining: event.target.options[event.target.selectedIndex].value
     });
-    }
   }
   startCountdown = () => {
-    if(!this.state.timerRunning) {
-      this.setState({timerRunning: true});
-      this.interval = setInterval(this.tick, 1000);
-    }
+    this.setState({timerRunning: true});
+    this.interval = setInterval(this.tick, 1000);
   }
   resetCountdown = () => {
     clearInterval(this.interval);
     this.setState({timerRunning: false, secondsRemaining: this.state.selectedSeconds});
-    this.startCountdown();
   }
   tick = () => {
     this.setState({secondsRemaining: this.state.secondsRemaining - 1});
     if (this.state.secondsRemaining <= 0) {
-      clearInterval(this.interval);
-      this.setState({timerRunning: false, secondsRemaining: this.state.selectedSeconds});
+      this.resetCountdown();
     }
   }
-	render = () => {
-    let button, progressBar, select;
-    if(!this.state.timerRunning) {
-      select = (
-        <Input type="select" value={this.state.selectedSeconds} onChange={this.setInitialTime}>
-          <option value="60">60 seconds</option>
-          <option value="120">120 seconds</option>
-          <option value="180">180 seconds</option>
-        </Input>
-      );
-      button = <Button block onClick={this.startCountdown}>Start Timer</Button>;
-    } else {
-      button = <Button block onClick={this.resetCountdown}>Stop Timer</Button>;
-      progressBar = <ProgressBar max={this.state.selectedSeconds} now={this.state.secondsRemaining} label={this.state.secondsRemaining}></ProgressBar>;
-    }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+	render() {
 		return (
       <div>
         <p>Stop slacking! Time your breaks!</p>
-          {select}
-          
-          {progressBar}
-          {button}
-          
+        {this.state.timerRunning
+          ?
+          <ProgressBar max={this.state.selectedSeconds} now={this.state.secondsRemaining} label={this.state.secondsRemaining} />
+          :
+          <Input type="select" value={this.state.selectedSeconds} onChange={this.setInitialTime}>
+            <option value="60">60 seconds</option>
+            <option value="120">120 seconds</option>
+            <option value="180">180 seconds</option>
+          </Input>
+        }
+        <Button block onClick={this.state.timerRunning ? this.resetCountdown : this.startCountdown}>
+          {this.state.timerRunning ? 'Stop' : 'Start'} Timer
+        </Button>
       </div>
 		);
 	}
