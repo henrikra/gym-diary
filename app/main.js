c23c3358b35031ec437b4d320f85eb5a0a18bb96
Henrik Raitasola <heka.fof@gmail.com>
2016-02-07 01:01:17 +0200

    Extracted routes to own file.

--- app/main.js
+++ app/main.js
@@ -1,37 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import Home from './components/Home';
-import Programs from './components/Programs';
-import Program from './components/Program';
-import Exercise from './components/Exercise';
-import Login from './components/Login';
-import Register from './components/Register';
-import { Router, Route, IndexRoute } from 'react-router';
+import { Router } from 'react-router';
 import createHistory from 'history/lib/createHashHistory';
-
-import auth from './auth';
+import routes from './routes';
 
 const history = createHistory({
   queryKey: false
 });
 
-const requireAuth = (nextState, replaceState) => {
-  if (!auth.loggedIn()) {
-    replaceState({ nextPathname: nextState.location.pathname }, '/login');
-  }
-};
-
 ReactDOM.render(
-	<Router history={history}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="login" component={Login} />
-      <Route path="register" component={Register} />
-      <Route path="programs" component={Programs} onEnter={requireAuth} />
-      <Route path="programs/:programId" component={Program} />
-      <Route path="exercises/:exerciseId" component={Exercise} />
-    </Route>
-  </Router>
+	<Router history={history} routes={routes} />
   , document.getElementById('root')
 );
---
+++ app/routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/App';
+import Home from './components/Home';
+import Programs from './components/Programs';
+import Program from './components/Program';
+import Exercise from './components/Exercise';
+import Login from './components/Login';
+import Register from './components/Register';
+
+import auth from './auth';
+
+const requireAuth = (nextState, replaceState) => {
+  if (!auth.loggedIn()) {
+    replaceState({ nextPathname: nextState.location.pathname }, '/login');
+  }
+};
+
+export default (
+	<Route path="/" component={App}>
+    <IndexRoute component={Home}/>
+    <Route path="login" component={Login} />
+    <Route path="register" component={Register} />
+    <Route path="programs" component={Programs} onEnter={requireAuth} />
+    <Route path="programs/:programId" component={Program} />
+    <Route path="exercises/:exerciseId" component={Exercise} />
+  </Route>
+);
